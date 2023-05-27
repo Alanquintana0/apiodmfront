02b76c94c8edc143aeda5a0f68558947b646a805
Alanquintana0 <65621524+Alanquintana0@users.noreply.github.com>
2023-05-27 14:53:30 -0600

    Agregado formulario para crear un proyecto

--- src/components/createProyect.vue
+++ src/components/createProyect.vue
@@ -1,16 +1,64 @@
 <template>
   <div class="container">
-    <h4>Hola</h4>
+    <h4>Crea un proyecto</h4>
     <div class="class-group">
       <label for="">Nombre del proyecto</label>
-      <input type="text" class="form-control" placeholder="Nombre del proyecto">
+      <input type="text" class="form-control" placeholder="Nombre del proyecto" v-model="ProyectRecords.name">
     </div>
+    <div class="class-group">
+      <label for="">Fecha de orden</label>
+      <input type="date" class="form-control" placeholder="Fecha en la que se pidio el proyecto" v-model="ProyectRecords.requestDate">
+    </div>
+    <div class="class-group">
+      <label for="">Fecha de inicio</label>
+      <input type="date" class="form-control" placeholder="Fecha en la que se inicio el proyecto" v-model="ProyectRecords.startDate">
+    </div>
+    <div class="class-group">
+      <label for="">Manager del proyecto</label>
+      <input type="text" class="form-control" placeholder="Product manager" v-model="ProyectRecords.proyectManager">
+    </div>
+    <div class="class-group">
+      <label for="">Owner del proyecto</label>
+      <input type="text" class="form-control" placeholder="Product Owner" v-model="ProyectRecords.productOwner">
+    </div>
+    <div class="class-group">
+      <label for="">Equipo de desarrollo</label>
+      <input type="text" class="form-control" placeholder="Equipo que trabajara en el proyecto" v-model="ProyectRecords.developmentTeam">
+    </div>
+    <label for="">Estado:</label>
+    <select v-model="ProyectRecords.status">
+      <option value="true">Activo</option>
+      <option value="false">Inactivo</option>
+    </select>
+    <button class="btn btn-large btn-block btn-success btn-primary full-width" @click="addToApi">Iniciar sesion</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'createProyect'
+  name: 'createProyect',
+  data () {
+    return {
+      ProyectRecords: {name: '', requestDate: '', startDate: '', proyectManager: '', productOwner: '', developmentTeam: [], description: '', status: true}
+    }
+  },
+  methods: {
+    addToApi () {
+      let newProyect = {
+        name: this.ProyectRecords.name,
+        requestDate: this.ProyectRecords.requestDate,
+        startDate: this.ProyectRecords.startDate,
+        proyectManager: this.ProyectRecords.proyectManager,
+        productOwner: this.ProyectRecords.productOwner,
+        developmentTeam: this.ProyectRecords.developmentTeam.push,
+        status: this.ProyectRecords.status
+      }
+      console.log(newProyect)
+      axios.post('http://localhost:3000/proyectRecords', newProyect).then((response) => { console.log(response) }).catch((error) => { console.log(`Si llega aqui ${error}`) })
+    }
+  }
 }
 </script>
 
