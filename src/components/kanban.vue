20b171a497560e768a091af46bd2f6f798c96aed
Edgar Gonzalez <a329927@uach.mx>
2023-05-28 19:38:53 -0600

    Bachlog

--- src/components/kanban.vue
+++ src/components/kanban.vue
@@ -1,36 +1,55 @@
 <template>
     <div>
       <h1>Backlog</h1>
-      <h2>List of Items:</h2>
-      <input type="text" v-model="busca" />
-      <button @click="buscar">Search</button>
-      <div class="space">
-      </div>
-      <div class="container">
-        <table class="custom-table">
+      <div class="table-container">
+        <h2>Tareas</h2>
+              <table class="custom-table">
   <thead>
     <tr>
       <th>Nombre</th>
       <th>Prioridad</th>
       <th>Estatus</th>
-      <th>Resultado</th>
-      <th>Acciones</th>
     </tr>
   </thead>
   <tbody>
-    <tr v-for="item in items" :key="item.id">
-      <td>{{ item._name }}</td>
-      <td>{{ item._priority }}</td>
-      <td>{{ item._status }}</td>
-      <td>{{ item._result }}</td>
-      <td>
-        <button class="edit-button" @click="editItem(item)">Editar Estatus</button>
-      </td>
-    </tr>
+    <tr v-for="item in items" :key="item.id" draggable="true" @dragstart="startDrag(item)">
+                <td>{{ item._name }}</td>
+                <td>{{ item._priority }}</td>
+                <td>{{ item._status }}</td>
+              </tr>
   </tbody>
 </table>
-
+<div class="table-container">
+        <div class="table-wrapper">
+            <h2>En Proceso</h2>
+            <!-- Encabezado de la tabla -->
+            <thead class="custom-table">
+    <tr>
+      <th>Nombre</th>
+      <th>Prioridad</th>
+      <th>Estatus</th>
+    </tr>
+            </thead>
         </div>
+    </div>
+      </div>
+      <!-- Repite el mismo patrón para las otras dos tablas (Backlog y Terminadas) -->
+
+      <div class="table-container">
+        <h2>Terminadas</h2>
+        <div class="table-wrapper" @dragover="allowDrop($event)" @drop="dropItem('backlogItems')">
+          <table class="custom-table">
+            <thead class="custom-table">
+    <tr>
+      <th>Nombre</th>
+      <th>Prioridad</th>
+      <th>Estatus</th>
+    </tr>
+            </thead>
+            <!-- Elementos de la tabla -->
+          </table>
+        </div>
+      </div>
     </div>
   </template>
 <script>
@@ -40,7 +59,11 @@ import axios from 'axios'
 export default {
   data () {
     return {
-      items: []
+      items: [],
+      porHacerItems: [],
+      backlogItems: [],
+      terminadasItems: [],
+      selectedItem: null
     }
   },
   mounted () {
@@ -55,31 +78,46 @@ export default {
     .catch(error => {
       console.error(error)
         })
-    },
-    buscar() {
-      this.filteredItems = this.items.filter(item =>
-        item._name.toLowerCase().includes(this.busca.toLowerCase())
-      )
-    },
-    editItem(item) {
-        const updatedItem = {
-        ...item,
-        _status: 'Nuevo estado', // Reemplaza 'Nuevo estado' por el nuevo valor de _status
-        }
-        axios.put('https://api1-a329927.b4a.run/userHistory', updatedItem)
-        .then(response => {
-            const index = this.items.findIndex(i => i.id === item.id)
-        if (index !== -1) {
-            this.items.splice(index, 1, updatedItem)
-        }
-        })
-        .catch(error => {
-        console.error(error)
-        })
     }
 }}
 </script>
 <style>
+.table-container {
+  margin-bottom: 30px;
+}
+
+.custom-table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-bottom: 10px;
+}
+
+.custom-table th,
+.custom-table td {
+  border: 1px solid #ccc;
+  padding: 8px;
+  text-align: center;
+}
+
+.table-wrapper {
+  min-height: 100px;
+  border: 2px dashed #ccc;
+  padding: 10px;
+}
+
+.edit-button {
+  margin-right: 5px;
+}
+
+/* Estilos para el elemento arrastrable */
+tr[draggable="true"] {
+  cursor: move;
+}
+
+/* Estilos para el contenedor de soltar */
+.table-wrapper.dragover {
+  background-color: #f9f9f9;
+}
 .custom-table {
   width: 100%;
   border-collapse: collapse;
@@ -111,7 +149,7 @@ export default {
 .edit-button:hover {
   background-color: #0056b3;
 }
-.space{
-    margin-top: 40px;
-}
+
+</style>
+
 </style>
